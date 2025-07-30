6be333ca9b99d7d2c979c2cc67c7a6acd6bf331d
Sumi Masahiro <sumi.masahiro312@mail.kyutech.jp>
2025-07-30 21:55:14 +0900

    Add VS Code settings and gitattributes, fix CSS lint warnings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
 
 export const metadata = {
   title:'東方求徹区',
